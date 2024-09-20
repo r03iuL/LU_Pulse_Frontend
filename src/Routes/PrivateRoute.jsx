301f6ca8ec4fc7d0f3a36a23636389aee1b44021
r03iuL <robiulislam1806@gmail.com>
2024-09-21 02:43:55 +0600

    Last Midnight_2

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,7 +5,7 @@ const PrivateRoute = ({ children }) => {
   const { currentUser } = useAuth(); // Access the current user from AuthContext
 
   // If the user is authenticated, render the element, otherwise, redirect to login
-  return currentUser ? element : <Navigate to="/login" />;
+  return currentUser ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
